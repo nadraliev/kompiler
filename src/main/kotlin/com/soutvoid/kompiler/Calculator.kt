d7dc1459c9c210f59f98b2c6e338477058f6798f
nadraliev <nadraliev@gmail.com>
2017-09-17 20:38:17 +0300

    added simple calculator

--- src/main/kotlin/com/soutvoid/kompiler/Calculator.kt
+++ src/main/kotlin/com/soutvoid/kompiler/Calculator.kt
@@ -2,19 +2,25 @@ package com.soutvoid.kompiler
 
 class Calculator: CalculatorBaseVisitor<Int>() {
 
-    override fun visitParenthesis(ctx: CalculatorParser.ParenthesisContext?): Int {
-        return super.visitParenthesis(ctx)
+    override fun visitParenthesis(ctx: CalculatorParser.ParenthesisContext?): Int? {
+        return visit(ctx?.expression())
     }
 
-    override fun visitMulDiv(ctx: CalculatorParser.MulDivContext?): Int {
-        return super.visitMulDiv(ctx)
+    override fun visitMulDiv(ctx: CalculatorParser.MulDivContext?): Int? {
+        return if (ctx?.MUL() != null)
+            visit(ctx.left) * visit(ctx.right)
+        else
+            visit(ctx?.left) / visit(ctx?.right)
     }
 
-    override fun visitAddSub(ctx: CalculatorParser.AddSubContext?): Int {
-        return super.visitAddSub(ctx)
+    override fun visitAddSub(ctx: CalculatorParser.AddSubContext?): Int? {
+        return if (ctx?.ADD() != null)
+            visit(ctx.left) + visit(ctx.right)
+        else
+            visit(ctx?.left) - visit(ctx?.right)
     }
 
-    override fun visitInt(ctx: CalculatorParser.IntContext?): Int {
-        return super.visitInt(ctx)
+    override fun visitInt(ctx: CalculatorParser.IntContext?): Int? {
+        return ctx?.INT()?.text?.toInt()
     }
 }
--- src/main/kotlin/com/soutvoid/kompiler/Hello.kt
+++ src/main/kotlin/com/soutvoid/kompiler/Hello.kt
@@ -1,8 +1,16 @@
 package com.soutvoid.kompiler
 
 import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
 
 fun main(args: Array<String>) {
-    println("Hello, World")
+    val expression = "(4+2)*4/3"
+    val stream = CharStreams.fromString(expression)
+    val lexer = CalculatorLexer(stream)
+    val tokenStream = CommonTokenStream(lexer)
+    val parser = CalculatorParser(tokenStream)
+    val tree = parser.expression()
+    val result = Calculator().visit(tree)
+    println(result)
 }
 
