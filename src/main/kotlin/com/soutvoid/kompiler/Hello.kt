3d18974b16cce558a01a9c45c3a24fc163d860f3
andrew <soutvoid@gmail.com>
2017-09-18 20:52:19 +0300

    added tree gui

--- src/main/kotlin/com/soutvoid/kompiler/Hello.kt
+++ src/main/kotlin/com/soutvoid/kompiler/Hello.kt
@@ -1,10 +1,17 @@
 package com.soutvoid.kompiler
 
+import org.antlr.v4.gui.TreeViewer
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import javax.swing.JFrame
+import javax.swing.JPanel
+import java.util.Arrays
+import java.util.Arrays.asList
+
+
 
 fun main(args: Array<String>) {
-    val expression = "(4+2)*4/3"
+    val expression = "456+98/2*(3-1)"
     val stream = CharStreams.fromString(expression)
     val lexer = CalculatorLexer(stream)
     val tokenStream = CommonTokenStream(lexer)
@@ -12,5 +19,12 @@ fun main(args: Array<String>) {
     val tree = parser.expression()
     val result = Calculator().visit(tree)
     println(result)
+
+//    val frame = JFrame("AST for expression: " + expression)
+//    val treeViewer = TreeViewer(parser.ruleNames.asList(), tree)
+//    treeViewer.scale = 1.5//scale a little
+//    frame.add(treeViewer)
+//    frame.setSize(640, 480)
+//    frame.isVisible = true
 }
 
