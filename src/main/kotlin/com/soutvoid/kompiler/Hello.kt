90a2aceacaa9f23a0e979d6390e8fc98f035a3d1
nadraliev <nadraliev@gmail.com>
2017-10-01 13:27:49 +0300

    now choosing source file from gui

--- src/main/kotlin/com/soutvoid/kompiler/Hello.kt
+++ src/main/kotlin/com/soutvoid/kompiler/Hello.kt
@@ -4,41 +4,43 @@ import io.bretty.console.tree.TreePrinter
 import org.antlr.v4.gui.TreeViewer
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.tree.Tree
+import javax.swing.JFileChooser
 import javax.swing.JFrame
 
 fun main(args: Array<String>) {
-    val expression =
-            "class Test{\n" +
-            "fun test() {\n" +
-                    "var foo: Int = 5\n" +
-                    "foo =7\n" +
-                    "if (1 < 0) {\n" +
-                    "var wrong: Boolean = true\n" +
-                    "}\n" +
-                    "}\n" +
-                    "\n" +
-                    "fun test2(param: Double) {\n" +
-                    "var boo: Boolean = 5 < 3\n" +
-                    "if (boo)\n" +
-                    "var right: Int = 3\n" +
-                    "}\n" +
-                    "}\n"
 
-    val stream = CharStreams.fromString(expression)
+    val filePath = openSource()
+    if (filePath.isEmpty())
+        return
+
+    val stream = CharStreams.fromFileName(filePath)
     val lexer = KotlinLexer(stream)
     val tokenStream = CommonTokenStream(lexer)
     val parser = KotlinParser(tokenStream)
     parser.addParseListener(KotlinListener())
     val tree = parser.classDeclaration()
 
-    val frame = JFrame("AST for expression: " + expression)
+    showSyntaxTree(parser, tree)
+
+    val treeAst = tree.toAst()
+    println(TreePrinter.toString(treeAst))
+}
+
+fun openSource(): String {
+    val jFileChooser = JFileChooser()
+    val returnVal = jFileChooser.showOpenDialog(null)
+    if (returnVal == JFileChooser.APPROVE_OPTION)
+        return jFileChooser.selectedFile.absolutePath
+    else return ""
+}
+
+fun showSyntaxTree(parser: KotlinParser, tree: Tree) {
+    val frame = JFrame("Syntax tree")
     val treeViewer = TreeViewer(parser.ruleNames.asList(), tree)
     treeViewer.scale = 1.5//scale a little
     frame.add(treeViewer)
     frame.setSize(640, 480)
     frame.isVisible = true
-
-    val treeAst = tree.toAst()
-    println(TreePrinter.toString(treeAst))
 }
 
